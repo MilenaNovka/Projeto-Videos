ecad884e13930b54a30df243a55c073856f401ae
Milena Novka <103338752+MilenaNovka@users.noreply.github.com>
2025-10-14 17:17:49 -0300

    Version 1.2

--- server.js
+++ server.js
@@ -4,4 +4,61 @@ import cors from "@fastify/cors";
 import fastifyStatic from "@fastify/static";
 import { fileURLToPath } from "url";
 import {dirname, join} from "path";
+import { request } from "http";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const server = fastify();
+
+// Serve arquivos estáticos da pasta 'public'
+await server.register(fastifyStatic, {
+    root: join(__dirname, "public"),
+    prefix: "/", // Para acessar os arquivos 
+});
+
+// Configura CORS
+await server.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+});
+
+const database = new DatabasePostgres();
+
+// Rotas
+server.post("/videos", async(request, reply) =>{
+    const {title, description, duration} = request.body;
+
+    await database.create({title, description, duration});
+
+    return reply.status(201).send();
+});
+
+server.get("/videos", async(request, reply) =>{
+    const search = request.query.search;
+    const videos = await database.list(search);
+
+    return videos; // Sempre retorna array
+});
+
+server.put("/videos/:id", async(request, reply) => {
+    const videoId = request.params.id;
+    const {title, description, duration} = request.body;
+
+    await database.update(videoId, {title, description, duration});
+
+    return reply.status(204).send();
+
+});
+
+server.delete("/videos/:id", async(request, reply) => {
+    const videoId = request.params.id;
+    
+    await database.delete(videoId);
+
+    return reply.status(204).send();
+});
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+}).then(() => console.log("sevidor rodando em http://localhost:3333"));
